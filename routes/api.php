64c9ac53ef3e6eea3048cc17a4bd98bc9b9dab29
James Paul Tulod <paultulod@pm.me>
2024-03-04 19:28:49 +0800

    Create API routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/artists', 'App\Http\Controllers\API\ArtistApiController@index');
+Route::get('/artists/{artist}', 'App\Http\Controllers\API\ArtistApiController@show');
+Route::post('/artists', 'App\Http\Controllers\API\ArtistApiController@store');
+Route::put('/artists/{artist}', 'App\Http\Controllers\API\ArtistApiController@update');
+Route::delete('/artists/{artist}', 'App\Http\Controllers\API\ArtistApiController@destroy');
+
+Route::get('/albums', 'App\Http\Controllers\API\AlbumApiController@index');
+Route::get('/albums/{album}', 'App\Http\Controllers\API\AlbumApiController@show');
+Route::post('/albums', 'App\Http\Controllers\API\AlbumApiController@store');
+Route::put('/albums/{album}', 'App\Http\Controllers\API\AlbumApiController@update');
+Route::delete('/albums/{album}', 'App\Http\Controllers\API\AlbumApiController@destroy');
